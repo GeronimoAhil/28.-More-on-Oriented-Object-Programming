1d30b090a833fa60b01622b41df6f7a0ecb67298
GeronimoAhil <geromy2014@gmail.com>
2026-03-03 16:01:04 +0800

    Add files via upload

--- Area and perimeter of a circle.py
+++ Area and perimeter of a circle.py
@@ -1,12 +1,12 @@
 class circle:
-    def __init__(self, radius):
+    def __init__(self, radius, area, perimeter):
         self.radius = radius
+        self.area = area
+        self.perimeter = perimeter
 radius = int(input("Please enter a number greater than 5, but less than 10: "))
-pi = 3.14
 
+pi = 3.14
 area = pi*radius**2
 perimeter = 2*pi*radius
-
-
-print("The area of the circle is ",area(int))
-print("The perimeter of the circle is ",perimeter(int))
+print("The area of the circle is ",area)
+print("The perimeter of the circle is ",perimeter)
